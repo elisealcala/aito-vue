6c70111c962255b09cfa6c94d2d5969d9aceedc7
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2023-05-26 17:39:57 -0500

    feat: adjust logic

--- pages/to-do.vue
+++ pages/to-do.vue
@@ -7,12 +7,29 @@
           <input v-model="text" placeholder="Enter a new todo" class="border border-none rounded px-3 text-sm" />
           <button class="bg-blue-700 text-white flex items-center justify-center p-3 rounded text-sm ml-4" type="submit">Add</button>
         </form>
-        <button class="bg-blue-500 text-white flex items-center justify-center px-3 py-2 rounded text-sm ml-4 text-sm" @click="removeAll">Remove All</button>
       </div>
-      <div class="mt-10">
-        <div v-for="item in todoItems" class="flex items-center py-4 border-b justify-between">
-          <span>{{item.content}}</span>
-          <button class="bg-blue-500 text-white flex items-center justify-center px-3 py-2 rounded text-sm ml-4 text-sm" @click="removeTodo(item)">Remove</button>
+      <div class="mt-10" v-if="todoItems.length > 0">
+        <div class="flex justify-evenly mb-4">
+          <div :class="view === 'all' ? 'bg-blue-100' : ''" class="border text-sm px-10 py-3 cursor-pointer" @click="() => view = 'all'">
+            <span>All</span>
+          </div>
+          <div v-if="completedItems.length > 0" :class="view === 'completed' ? 'bg-blue-100' : ''"  class="flex text-sm ml-4 border px-10 py-3 cursor-pointer" @click="() => view = 'completed'">
+            <span>Completed</span>
+          </div>
+        </div>
+        <div v-if="view === 'all'" v-for="item in todoItems" class="flex items-center py-4 border-b justify-between">
+          <span :class="completedItems.some(i => i.id === item.id) ? 'line-through text-slate-400': ''">{{item.content}}</span>
+          <div class="flex">
+            <button v-if="!completedItems.some(i => i.id === item.id)" class="bg-green-400 text-white flex items-center justify-center px-3 py-2 rounded text-sm ml-4 text-sm" @click="completeItem(item)">Complete</button>
+            <button class="bg-red-400 text-white flex items-center justify-center px-3 py-2 rounded text-sm ml-4 text-sm" @click="removeTodo(item)">Remove</button>
+          </div>
+        </div>
+        <div v-else>
+          <div v-for="item in completedItems" class="flex items-center py-4 border-b justify-between">
+            <span class="line-through text-slate-400">{{item.content}}</span>
+            <button class="bg-red-400 text-white flex items-center justify-center px-3 py-2 rounded text-sm ml-4 text-sm" @click="removeTodoFromCompleted(item)">Not Completed</button>
+          </div>
+          <button class="bg-red-400 text-white flex items-center justify-center px-3 py-2 rounded text-sm mt-6 text-sm" @click="removeAllCompleted">Remove All Completed</button>
         </div>
       </div>
     </section>
@@ -20,21 +37,25 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
 
   type Todo = {
     id: string,
     content: string
+    isCompleted: boolean
   }
 
-  const text = ref("" as string)
+  const view = ref("all")
+  const text = ref("")
   const todoItems = ref([] as Todo[])
+  const completedItems = computed(() => todoItems.value.filter(i => i.isCompleted))
 
   const addNewItem = () => {
     if (text.value.length > 2) {
       todoItems.value.push({
         id: Math.floor((Math.random() * 10) + 1) + text.value[0] + text.value[1],
-        content: text.value
+        content: text.value,
+        isCompleted: false,
       })
       text.value = ""
     }
@@ -44,9 +65,29 @@
     todoItems.value = todoItems.value.filter(({ id }) => id !== todoId)
   }
 
-  const removeAll = () => {
-    todoItems.value = []
+  const removeTodoFromCompleted = ({ id: todoId }: Todo) => {
+    todoItems.value = 
+      todoItems.value.map((item) => item.id === todoId ? ({
+        ...item,
+        isCompleted: false
+      }) : item 
+    )
+  }
+
+  const completeItem = (todo: Todo) => {
+    todoItems.value = 
+      todoItems.value.map((item) => item.id === todo.id ? ({
+        ...item,
+        isCompleted: true
+      }) : item 
+    )
+  }
+
+  const removeAllCompleted = () => {
+    todoItems.value =
+      todoItems.value.filter((item) => !item.isCompleted)
     text.value = ""
+    view.value = 'all'
   }
 
 </script>
